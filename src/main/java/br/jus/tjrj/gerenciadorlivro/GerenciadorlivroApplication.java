37fa40e5ffefcfd43f7fe580f854b3acc2f3df21
Rayldon <r4yldon@gmail.com>
2024-11-28 15:58:43 -0300

    feat: estrutura inicial

---
+++ src/main/java/br/jus/tjrj/gerenciadorlivro/GerenciadorlivroApplication.java
@@ -0,0 +1,13 @@
+package br.jus.tjrj.gerenciadorlivro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorlivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorlivroApplication.class, args);
+	}
+
+}
