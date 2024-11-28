9331746d8a91d03d2acde2902ce0286a0da77aa0
Rayldon <r4yldon@gmail.com>
2024-11-28 20:28:26 -0300

    feat: implementar execução de scripts com flyway

---
+++ src/main/resources/db/migration/V1__criar_tabelas.sql
@@ -0,0 +1,33 @@
+CREATE TABLE Livro (
+   Codl INTEGER PRIMARY KEY,
+   Titulo VARCHAR(40),
+   Editora VARCHAR(40),
+   Edicao INTEGER,
+   AnoPublicacao VARCHAR(4)
+);
+
+CREATE TABLE Autor (
+   CodAu INTEGER PRIMARY KEY,
+   Nome VARCHAR(40)
+);
+
+CREATE TABLE Livro_Autor (
+     Livro_Cod INTEGER,
+     Autor_CodAu INTEGER,
+     PRIMARY KEY (Livro_Cod, Autor_CodAu),
+     CONSTRAINT fk_livro FOREIGN KEY (Livro_Cod) REFERENCES Livro (Codl) ON DELETE CASCADE,
+     CONSTRAINT fk_autor FOREIGN KEY (Autor_CodAu) REFERENCES Autor (CodAu) ON DELETE CASCADE
+);
+
+CREATE TABLE Assunto (
+     codAs INTEGER PRIMARY KEY,
+     Descricao VARCHAR(20)
+);
+
+CREATE TABLE Livro_Assunto (
+   Livro_Cod INTEGER,
+   Assunto_codAs INTEGER,
+   PRIMARY KEY (Livro_Cod, Assunto_codAs),
+   CONSTRAINT fk_livro_assunto FOREIGN KEY (Livro_Cod) REFERENCES Livro (Codl) ON DELETE CASCADE,
+   CONSTRAINT fk_assunto FOREIGN KEY (Assunto_codAs) REFERENCES Assunto (codAs) ON DELETE CASCADE
+);
